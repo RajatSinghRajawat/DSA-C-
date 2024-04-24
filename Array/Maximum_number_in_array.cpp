32acafb148e2ef3f126717f5061e3ee576f7f2a5
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2024-04-24 11:41:09 +0530

    added 5 questions

---
+++ Array/Maximum_number_in_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // method one
+    // int a[5] = { 0, 1, 2, 3, 4};
+    // int max_num = a[0];
+    // for(int i = 1; i < 5; i++){
+    //     if(a[i] > max_num){
+    //         max_num = a[i];
+    //     }
+    // }
+    // cout << max_num << endl;
+
+    // method two
+
+    // int a[5] = { 0, 1, 2, 3, 4};
+    // int max_num = -1;
+    // for(int i = 1; i < 5; i++){
+    //     if(a[i] > max_num){
+    //         max_num = a[i];
+    //     }
+    // }
+    // cout<<"mac"<<max_num;
+
+    // find minimum number in our array
+    // int a[5] = { 0, 1, 2, 3, 4};
+    // int min_num = +1;
+    // for(int i = 1; i < 5; i++){
+    //     if(a[i] > max_num){
+    //         max_num = a[i];
+    //     }
+    // }
+    // cout<<"mac"<<max_num;
+}
---
+++ Array/Sorting_array.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+
+using namespace std;
+
+// int main()
+// {
+
+//     int n;
+//     cout << "enter maximum 5 values ";
+//     cin >> n;
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << "enter your values"
+//              << " ";
+//         cin >> a[i];
+//     }
+//     int start;
+//     int end = n - 1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] > start)
+//         {
+//             start = a[i];
+//         }
+//         if (a[i] < end)
+//         {
+//             end = a[i];
+//         }
+//     }
+//     // start++;
+
+//     // end--;
+
+//     cout << "end value"
+//          << " " << start << "start value"
+//          << " " << end;
+// }
+
+// int main(){
+//     int size = 5;
+//     int myarr[size] = {1,2,3,4,5};
+//     int start = 0;
+//     int end = size - 1;
+//     while(true){
+//         if(start>end){
+//             break;
+//         }else{
+//             cout<<myarr[start]<<endl;
+//             start++;
+//             cout<<myarr[end]<<endl;
+//             end--;
+//         }
+//     }
+
+// }
+
+
+int main(){
+//     int size = 5;
+//     int myarr[size] = {1,2,3,4,5};
+//     int start = 0; 
+//     int medium = size/2;
+//     int end = size - 1;
+//     for(int i=0; i<size; i++){
+//         if(start>end){
+//             break;
+//         }else{
+//             if(i==medium){
+//                 cout<<myarr[start]<<endl;
+//                 start++;
+
+//             }else{
+//                 cout<<myarr[start]<<endl;
+//                 start++;
+//                 cout<<myarr[end]<<endl;
+//                 end--;
+
+//             }
+//         }
+
+// }
+//   int size;
+//   cout << "Enter the size of the array: ";
+//   cin >> size;
+
+//   int myarr[size];
+//   cout << "Enter the elements of the array: ";
+//   for (int i = 0; i < size; i++) {
+//       cin >> myarr[i];
+//   }
+
+//   int start = 0; 
+//   int medium = size / 2;
+//   int end = size - 1;
+//   for (int i = 0; i < size; i++) {
+//       if (start > end) {
+//           break;
+//       } else {
+//           if (i == medium) {
+//               cout << myarr[start] << endl;
+//               start++;
+//           } else {
+//               cout << myarr[start] << endl;
+//               start++;
+//               cout << myarr[end] << endl;
+//               end--;
+//           }
+//       }
+//   }
+
+
+}
