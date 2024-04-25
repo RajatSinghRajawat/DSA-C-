e2adeead588fe46190d23615727c4a0f374f73f2
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2024-04-25 11:56:46 +0530

    vector question

---
+++ Vector/Simple_vector.cpp
@@ -0,0 +1,45 @@
+
+//   #include <iostream>
+//   #include <vector>
+//   using namespace std;
+
+//   int main(){
+//       vector<int>elements;
+//       elements.push_back(7);
+//       elements.push_back(10);
+//       cout <<elements.size();
+//       elements.pop_back();
+//       for(int i=0; i<elements.size(); i++){
+//           cout<<elements[i];
+//       }
+
+//   }
+
+
+// take vetor userinput lenthe and than print vector
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> elements;
+//     int length;
+
+//     cout << "Enter the length of the vector: ";
+//     cin >> length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         int element;
+//         cin >> element;
+//         elements.push_back(element);
+//     }
+//     for (int i = 0; i < elements.size(); i++)
+//     {
+//         cout << elements[i] << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
