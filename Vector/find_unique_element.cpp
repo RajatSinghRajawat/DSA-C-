09e4b2a9b07ca630af35e55ab9aa642c7bd2a16b
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2024-05-03 11:34:44 +0530

    implemented bruteforce approach for find unique elements from an vector

---
+++ Vector/find_unique_element.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findUniqueElement(vector<int> values){
+    int uniqueValue = 0;
+    for(int i=0;i<values.size();i++){
+        uniqueValue = uniqueValue ^ values[i];
+    }
+    return uniqueValue;
+}
+
+int main(){
+    vector<int>values = {1,1,5,4,4};
+    int uniqueValue = findUniqueElement(values);
+    cout<<"Unique Value : "<<uniqueValue<<endl;
+}
