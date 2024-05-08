0e5299aacc9f23a934a7acdca22e1d51d2cc50eb
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2024-05-08 20:21:43 +0530

    new one

--- Searching/BinarySearching.cpp
+++ Searching/BinarySearching.cpp
@@ -45,50 +45,50 @@
 
 // TAKE USERINPUT
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int binary_search(int arr[],int size , int target)
-{
-    int start = 0;
-    int end = size-1;
-    int mid = start+end/2;
+// int binary_search(int arr[],int size , int target)
+// {
+//     int start = 0;
+//     int end = size-1;
+//     int mid = start+end/2;
 
-    while(start<=end){
-        if(arr[mid]==target){
-            return mid;
-        }else if(arr[mid]<target){
-            start = mid + 1;
-        }
-        else{
-            end = mid -1;
+//     while(start<=end){
+//         if(arr[mid]==target){
+//             return mid;
+//         }else if(arr[mid]<target){
+//             start = mid + 1;
+//         }
+//         else{
+//             end = mid -1;
 
-        }
-        mid = start + mid / 2;;
-    }
+//         }
+//         mid = start + mid / 2;;
+//     }
 
-}
-int main()
-{
+// }
+// int main()
+// {
 
-    int size;
-    cout << "Enter your  Size number : ";
-    cin >> size;
+//     int size;
+//     cout << "Enter your  Size number : ";
+//     cin >> size;
 
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        cout << "enter your array value: ";
-        cin >> arr[i];
-    }
+//     int arr[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << "enter your array value: ";
+//         cin >> arr[i];
+//     }
 
-    int target;
-    cout << "enter your target value: ";
-    cin >> target;
-    int indexvalue = binary_search(arr , size , target);
-    if(indexvalue == -1){
-        cout<<"no value found";
-    }else{
-        cout<<" Mid value is this: " <<arr[indexvalue];
-    }           
-}
+//     int target;
+//     cout << "enter your target value: ";
+//     cin >> target;
+//     int indexvalue = binary_search(arr , size , target);
+//     if(indexvalue == -1){
+//         cout<<"no value found";
+//     }else{
+//         cout<<" Mid value is this: " <<arr[indexvalue];
+//     }           
+// }
